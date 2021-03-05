5355bf69a10936f5cd3af9a5a852d039d364d0db
Faisal Al-Shehri <61276435+FaisalRAlShehri@users.noreply.github.com>
2021-03-05 20:07:27 +0300

    Add files via upload

---
+++ Guess the secret number.py
@@ -0,0 +1,21 @@
+﻿from random import randint
+
+secret_number =  randint(0, 100)
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you between 0 & 100.|
+| So, what is the secret number? |
++================================+
+""")
+
+number = int(input("Enter the number: "))
+
+while number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    number = int(input("Enter the number again: "))
+print(secret_number, "Well done, muggle! You are free now.")
---
+++ Logical BMI Calculator.py
@@ -0,0 +1,9 @@
+﻿def bmi(weight, height):
+    if weight < 20 or weight > 220 or height < 1.00 or height > 2.50:
+        return "The data you entered are logically incorrect"
+    else:
+        return weight / height ** 2
+
+weight = float(input("Enter your Weight (In Kilograms): "))
+height = float(input("Enter your Height (In Meters): "))
+print(bmi(weight, height))
